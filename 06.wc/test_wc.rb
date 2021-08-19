4eae20885cc22b20c0f4fdcc5c49c8fd260695b3
ahira3 <hiraatsu.13096@gmail.com>
2021-08-19 22:18:09 +0900

    change method name

--- 06.wc/test_wc.rb
+++ 06.wc/test_wc.rb
@@ -8,26 +8,26 @@ class WcTest < Minitest::Test
     @input = File.read('a.txt')
   end
 
-  def test_load_number_of_line
-    assert_equal 3, load_number_of_line(@input)
+  def test_calc_number_of_line
+    assert_equal 3, calc_number_of_line(@input)
   end
 
-  def test_load_number_of_word
-    assert_equal 4, load_number_of_word(@input)
+  def test_calc_number_of_word
+    assert_equal 4, calc_number_of_word(@input)
   end
 
-  def test_load_file_size
-    assert_equal 26, load_file_size(@input)
+  def test_calc_file_size
+    assert_equal 26, calc_file_size(@input)
   end
 
-  def test_load_all_data
+  def test_calc_all_data
     expected = ['       3', '       4', '      26']
-    assert_equal expected, load_all_data(@input)
+    assert_equal expected, calc_all_data(@input)
   end
 
-  def test_load_some_file
-    files = ['a.txt', 'ls.rb']
-    expected = [['       3', '       4', '      26', ' a.txt'], ['     114', '     276', '    2834', ' ls.rb'], ['     117', '     280', '    2860', ' total']]
-    assert_equal expected, load_some_files(files)
+  def test_calc_some_file
+    files = ['a.txt', 'b.txt']
+    expected = [['       3', '       4', '      26', ' a.txt'], ['       6', '       8', '      54', ' b.txt'], ['       9', '      12', '      80', ' total']]
+    assert_equal expected, calc_some_files(files)
   end
 end
